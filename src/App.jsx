cb3cf120de637256c5002e4555550c86e34b03ab
Nitish Nashine <nitishnashine@gmail.com>
2024-11-14 12:07:27 +0530

    content added in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import { Stack } from "@mui/material";
 import React from "react";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Stack 
+      spacing={2}
+      justifyContent="center"
+      my={4}
+      color={"blue "}
+    >
+      <h1>Hello from Augmented</h1>
+    </Stack>
+  )
 };
 
 export default App;
