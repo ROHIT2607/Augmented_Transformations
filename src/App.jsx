8b91909f920c98f5ffe0b7f32bd92d51de74214a
Nitish Nashine <nitishnashine@gmail.com>
2024-11-14 16:46:28 +0530

    web design done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import { Stack } from "@mui/material";
 import React from "react";
+import Carousel from "./components/Carousel/Carousel";
+
 
 const App = () => {
   return (
-    <Stack 
-      spacing={2}
-      justifyContent="center"
-      my={4}
-      color={"blue "}
-    >
-      <h1>Hello from Augmented</h1>
+    <Stack>
+      <Carousel/>
     </Stack>
   )
 };
