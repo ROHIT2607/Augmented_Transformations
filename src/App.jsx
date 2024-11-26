3426f336ff2f9d2238611b54d2e97d70e9c09337
Nitish Nashine <nitishnashine@gmail.com>
2024-11-26 10:39:25 +0530

    Add react-router-dom for routing; refactor App component to use BrowserRouter and Routes; enhance Contact component with form and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Common/Header/Header";
 import IndustryPage from "./components/Industry_Page/IndustryPage";
 import VisionPage from "./components/VisionPage/VisionPage";
@@ -6,27 +7,34 @@ import Aihub from "./components/aihub/Aihub";
 import AIHubSection from "./components/aihub/Ai1hub";
 import AI1HubSection from "./components/aihub/Ai2hub";
 import Ourwork from "./components/ourWork/ourWork";
-
 import Projects from "./components/projects/Projects";
 import Footer from "./components/Footer/Footer";
 import SmartMetering from "./components/aihub/Smartmetering";
 
 const App = () => {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      {/* <CarAnimate/> */}
-      <IndustryPage />
-      <Aihub />
-      <AIHubSection/>
-      <AI1HubSection/>
-       <VisionPage />
-      <Ourwork/>
-      <SmartMetering/>
-     <Projects />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <IndustryPage />
+              <Aihub />
+              <AIHubSection />
+              <AI1HubSection />
+              <VisionPage />
+              <Ourwork />
+              <SmartMetering />
+              <Projects />
+            </>
+          }
+        />
+        {/* <Route path="/industry" element={} /> */}
+      </Routes>
       <Footer />
-
-    </>
+    </BrowserRouter>
   );
 };
 
