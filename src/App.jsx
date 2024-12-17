d9f49530c4a4bd679c3d62ff922bebcb36c6ee58
Nitish Nashine <nitishnashine@gmail.com>
2024-12-17 20:10:11 +0530

    Uncomment Footer component in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -45,7 +45,7 @@ const App = () => {
         <Route path="/contact" element={<ContactUs />} />
         <Route path="/products" element={<CobineProd/>}/>
       </Routes>
-      {/* <Footer /> */}
+      <Footer />
     </BrowserRouter>
   );
 };
